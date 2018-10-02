c2360b9b1fa71bcb7a8a5c489c51a6c2483cba52
Joke Heyndels <jo_hannah@outlook.com>
2018-10-02 13:49:44 +0200

    answer check works :)

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 import os
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 import json
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def show_riddles():
   data = []
-  question_number=20
+  question_number=1
   with open("data/riddles.json", "r", encoding="utf-8") as riddle_data:
     data = json.load(riddle_data)["riddles"]
   return render_template('riddle.html', riddles = data, question_number=question_number)
 
 
+@app.route("/", methods=["POST"])
+def check_answer():
+  correct_answer=request.form.get("correct_answer")
+  user_answer=request.form.get("guess")
+  if correct_answer in user_answer:
+    message="correct!"
+  else:
+    message="wrong!"
+  return render_template("answer.html", message=message, user_answer=user_answer, correct_answer=correct_answer)
       
 
 
