222c517dc9fd3cfb69463c5c81964c171172de80
Joke Heyndels <jo_hannah@outlook.com>
2018-10-02 10:35:40 +0200

    riddles are displayed

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import os
-from flask import Flask, render_template, redirect
-
+from flask import Flask, render_template, redirect, request
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-  message = "Hello World"
-  return render_template('riddle.html', message=message)
+def show_riddles():
+  data = []
+  question_number=20
+  with open("data/riddles.json", "r", encoding="utf-8") as riddle_data:
+    data = json.load(riddle_data)["riddles"]
+  return render_template('riddle.html', riddles = data, question_number=question_number)
+
+
+      
+
+
 
 if __name__ == "__main__":
   app.run(host=os.getenv("IP"),
