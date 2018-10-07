dafc149e8ec704936fdc54fa01d085db64a6a3d7
Joke Heyndels <jo_hannah@outlook.com>
2018-10-07 10:13:14 +0200

    Added 404 page

--- app.py
+++ app.py
@@ -159,6 +159,11 @@ def log_out():
     return render_template("loggedout.html")
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(host=os.getenv("IP"),
             port=os.getenv("PORT"),
