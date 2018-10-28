deb1e2a01ef648b837e653e09874367d3696179d
Joke Heyndels <jo_hannah@outlook.com>
2018-10-28 08:58:25 +0100

    added docstrings to app.py, removed superfluous function

--- app.py
+++ app.py
@@ -7,16 +7,36 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 
+"""HELPER FUNCTIONS"""
+
+"""Error Handlers"""
+"""Page Not Found"""
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+"""Server Error"""
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
+
+"""APP ROUTES"""
+
+"""Displays Index Page"""
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+"""Displays Register Page"""
 @app.route("/register")
 def show_register():
     return render_template("register.html")
 
 
+"""Writes User Input To File; Redirects to Riddles"""
 @app.route("/register", methods=["POST"])
 def register():
     player = request.form["new_user"].lower()
@@ -36,16 +56,18 @@ def register():
             session['score'] = start_score
             session['question_number'] = start_question_number
             session['riddles'] = riddles_list
-            return redirect(f"/riddles")
+            return redirect(url_for("show_riddle"))
         return render_template("register.html",
                                register_message=message)
 
 
+"""Displays Log-In Page"""
 @app.route("/signin")
 def show_signin():
     return render_template("signin.html")
 
 
+"Create session for registered user, shows warning if user is not yet registered."
 @app.route("/signin", methods=["POST"])
 def sign_in():
     player = request.form["username"].lower()
@@ -61,25 +83,27 @@ def sign_in():
             session['score'] = start_score
             session['question_number'] = start_question_number
             session['riddles'] = riddles_list
-            return redirect(f"/riddles")
+            return redirect(f"/riddle")
         else:
             message = "Sorry, this username is incorrect. New user? "
             return render_template("signin.html", signin_message=message)
 
 
+"""Resets score and question number and creates new riddle list if user in session wants to play again"""
 @app.route("/playagain")
-def play_again():
+def reset():
     with open("data/riddles.json", "r", encoding="utf-8)") as riddle_data:
         riddles_list = json.load(riddle_data)["riddles"]
         random.shuffle(riddles_list)
     session['score'] = 0
     session['question_number'] = 1
     session['riddles'] = riddles_list
-    return redirect(f"/riddles")
+    return redirect(f"/riddle")
 
 
-@app.route("/riddles")
-def show_riddles():
+"""Displays Riddle Page"""
+@app.route("/riddle")
+def show_riddle():
     if session:
         question_number = session['question_number']
         data = session['riddles']
@@ -92,7 +116,8 @@ def show_riddles():
         return redirect(url_for("index"))
 
 
-@app.route("/riddles", methods=["POST"])
+"""Compares user's answer with expected keyword for correct answer"""
+@app.route("/riddle", methods=["POST"])
 def check_answer():
     if session:
         session['correct_answer'] = request.form.get("correct_answer")
@@ -108,20 +133,22 @@ def check_answer():
         return redirect(url_for("index"))
 
 
+"""Allows user to skip question and immediately proceed to the next riddle"""
 @app.route("/skip_question")
 def skip():
     if session:
         session['question_number'] += 1
         if session['question_number'] >= 11:
-            return write_to_LB()
+            return redirect(url_for("write_to_LB"))
         else:
             session['message'] = "skipped"
-            return next_question()
+            return redirect(url_for("show_riddle"))
     else:
         return redirect(url_for("index"))
 
 
-@app.route("/answers")
+"""Displays Answer Page"""
+@app.route("/answer")
 def answer_result():
     if session:
         return render_template("answer.html",
@@ -135,63 +162,47 @@ def answer_result():
         return redirect(url_for("index"))
 
 
-@app.route("/answers", methods=["POST"])
-def answer_buttons():
+
+@app.route("/answer_redirect", methods=["POST"])
+def redirecting_from_answer_page():
     if session:
         if session['question_number'] >= 11:
-            write_to_LB()
+            return redirect(url_for("write_to_LB"))
         else:
-            next_question()
+            return redirect(url_for("show_riddle"))
     else:
         return redirect(url_for("index"))
 
 
-@app.route("/next_question", methods=["POST"])
-def next_question():
-    return redirect(url_for('show_riddles'))
-
-
-@app.route("/view_leaderboard", methods=["GET", "POST"])
+"""Writes username and score to leaderboard at end of game"""
+@app.route("/leaderboard", methods=["GET", "POST"])
 def write_to_LB():
     with open("data/score.json", "r") as score_data:
         player_score = {"user": session['user'], "score": session['score']}
         leaderboard = json.load(score_data)
         leaderboard["users"].append(player_score)
-        with open("data/score.json", "w") as score_data_updated:
-            json.dump(leaderboard, score_data_updated, indent=2)
+        with open("data/score.json", "w") as score_data:
+            json.dump(leaderboard, score_data, indent=2)
     return redirect(url_for('show_LB'))
 
 
-@app.route("/leaderboard")
+"""Displays Leaderboard Page"""
+@app.route("/view_leaderboard")
 def show_LB():
     with open("data/score.json", "r", encoding="utf-8") as score_data:
         data = json.load(score_data)["users"]
     return render_template("leaderboard.html", scores=data)
 
 
+"""Clears Session And Displays Log-Out Page"""
 @app.route("/log_out")
 def log_out():
-    session.pop("user", None)
-    session.pop("question_number", None)
-    session.pop("score", None)
-    session.pop("riddles", None)
-    session.pop("message", None)
-    session.pop("user_answer", None)
-    session.pop("correct_answer", None)
+    session.clear()
     return render_template("loggedout.html")
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def server_error(e):
-    return render_template('500.html'), 500
-
-
+"""Runs application"""
 if __name__ == "__main__":
     app.run(host=os.getenv("IP"),
             port=os.getenv("PORT"),
-            debug=False)
+            debug=True)
